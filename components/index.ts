c8824f9ddc77c6655ffb77a7b6b71a723ab90e4e
Foiering Sergey <serjofoi@gmail.com>
2022-10-29 18:47:55 +0300

    Active Link

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from './navbar';
 export { default as BaseLayout } from './layout/BaseLayout';
 export { default as NFTlist } from './nft/list';
 export { default as NFTitem } from './nft/item';
+export { default as ActiveLink } from './link';
