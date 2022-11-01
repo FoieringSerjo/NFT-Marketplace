4ec306c54b260180d3b91e09088cbb7624afec82
Foiering Sergey <serjofoi@gmail.com>
2022-11-01 19:11:07 +0200

    Web3 State+Type, openzeppelin/contracts, ERC721

---
+++ components/hooks/web3/useAccount.ts
@@ -0,0 +1,4 @@
+import useSWR from 'swr';
+
+//? deps -> provider, ethereum, contract (web3State)
+export const hookFactory = ({ ...deps }) => {};
--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,13 +1,39 @@
-import { createContext, FunctionComponent, useState, useContext } from 'react';
+import {
+  createContext,
+  FunctionComponent,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+import { createDefaultState, loadContract, Web3State } from './utils';
+import { ethers } from 'ethers';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Web3Context = createContext<any>(null);
+const Web3Context = createContext<Web3State>(createDefaultState());
 
 const Web3Provider: FunctionComponent<Props> = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({ test: 'Hello Provider!' });
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+  useEffect(() => {
+    async function initWeb3() {
+      const provider = new ethers.providers.Web3Provider(
+        window.ethereum as any
+      );
+      const contract = await loadContract('NftMarket', provider);
+
+      setWeb3Api({
+        ethereum: window.ethereum,
+        provider,
+        contract,
+        isLoading: false,
+      });
+    }
+
+    initWeb3();
+  }, []);
 
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
