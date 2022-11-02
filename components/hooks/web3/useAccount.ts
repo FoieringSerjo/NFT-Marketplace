4359148495023851f83676935578406490b925e5
Foiering Sergey <serjofoi@gmail.com>
2022-11-02 19:39:18 +0200

    useAccount hook with swr and testing

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,4 +1,15 @@
 import useSWR from 'swr';
 
 //? deps -> provider, ethereum, contract (web3State)
-export const hookFactory = ({ ...deps }) => {};
+export const hookFactory = (deps: any) => (params: any) => {
+  const swrRes = useSWR('web3/useAccount', () => {
+    // debugger;
+    console.log(deps);
+    console.log(params);
+    return 'Test User';
+  });
+
+  return swrRes;
+};
+
+export const useAccount = hookFactory({ ethereum: null, provider: null });
